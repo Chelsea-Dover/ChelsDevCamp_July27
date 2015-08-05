50c13ac480d48504976493ab1d0554773403df23
Chelsea <Chelseamegandover@gmail.com>
2015-08-05 16:19:29 -0700

    init commit of card class
    Card class prints what card is and if card is hidden or not

--- blackjack/card.py
+++ blackjack/card.py
@@ -3,11 +3,21 @@ class Card:
 	Class of cards
 	"""
 
-	def __init__(self, face_value, suit, color):
+	def __init__(self, face_value, suit, color, face_cards=[], is_hidden=True):
 		self.face_value = face_value
 #		self.real_value = real_value
 		self.suit = suit
 		self.color = color
+		self.is_hidden = is_hidden
+		self.face_cards = face_cards
+
+	def __repr__(self):
+		if self.is_hidden:
+			pretty_card = "This card is hidden"
+		else:
+			pretty_card = str(self.face_value) + " of " + str(self.suit)
+		return pretty_card
+
 
 	def cheat(self, face_value):
 		"""
@@ -15,13 +25,9 @@ class Card:
 		"""
 		pass
 
-	def face_up(self):
+	def _real_value(self):
 		"""
-		returns True cards is face up
-		"""
-
-	def print_card(self):
-		"""
-		pretty prints the card.
+		TODO next match face cards to real values
 		"""
 		pass
+
