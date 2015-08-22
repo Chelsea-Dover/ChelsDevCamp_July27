a9da956b16a3c032c269aa0c5f376a834acac495
Chelsea <Chelseamegandover@gmail.com>
2015-08-21 20:07:06 -0700

    modifications complete to test make_board

--- Conect4_game/Tests/test_model_make_board.py
+++ Conect4_game/Tests/test_model_make_board.py
@@ -9,40 +9,20 @@ class TestModelMakeBoard(unittest.TestCase):
 
     def setUp(self):
         """Inits empty grid for testing"""
-        self.grid = []
         self.test_grid = Model()
 
     def tearDown(self):
         """ Closes the model """
         del self.test_grid
 
-    def test_print_empty_list(self):
+    def test_make_board_length_7(self):
         """makes sure grid is empty"""
+        self.assertEqual(7, len(self.test_grid.grid), "len_should_be_seven")
 
-        self.assertEqual(0, len(self.test_grid.grid), "len_should_be_zero")
+    def test_make_board_inner_list_length_6(self):
+        """ confirms the zero index length equal to 6 """
+        self.assertEqual(6, len(self.test_grid.grid[0]))
 
-    def test_append_item_to_list(self):
-        """Append item to grid. Test grid length"""
-        self.grid.append("x")
-
-        self.assertEqual(1, len(self.grid))
-    #
-    # def test_make_row(self):
-    #     """Appends whole row to grid"""
-    #     for row in range(7):
-    #         self.grid.append("_")
-    #
-    #     res = "|".join(self.grid)
-    #
-    #     #print(res)
-    #
-    #     self.assertEqual(7, len(self.grid))
-
-    def test_make_board(self):
-        """Appends whole board to grid"""
-        self.test_grid.make_board()
-
-        self.assertEqual(48, len(self.test_grid.grid))
 
 if __name__ == '__main__':
     unittest.main()
