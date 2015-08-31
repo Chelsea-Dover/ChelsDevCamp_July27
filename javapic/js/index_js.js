8e480990f1de8fb53c86c21ce66dc3bbb4e72d33
Chelsea <Chelseamegandover@gmail.com>
2015-08-31 16:40:12 -0700

    Finished Javapic

--- javapic/js/index_js.js
+++ javapic/js/index_js.js
@@ -1,28 +1,7 @@
 /**
  * Created by Chelsea on 8/27/15.
  */
-//Makes the Jumbo-tron run through an amount of the photos from the file images
-//var jumboPictures = document.getElementById('jumbotron')
-//var file2 ="images/pdxcg*.jpg";
-//setInterval(function(){ alert("Test!"); }, 20000);
-//loop (function() {
-//        if jumboPictures[]
-//    }
-//)
-//
-//for (var i=0; i < jumboPictures.length; i++)
-
-
-//var timer = setTimeOut(newImage, 200000);
-//function makeAlt() {
-//    var images = document.getElementsByTagName('img');
-//    var numImages = images.length;
-//    for (i = 0; i < numImages; i++) {
-//        images[i].alt = images[i].src;
-//        images[i].title = images[i].src;
-//    }
-//}
-setInterval(function(){ changebg(); }, 2000);
+setInterval(function(){ changebg(); }, 20000);
 
 var bgImg = document.getElementById("jumbotron");
 //console.log(bgImg.bgImg.style.backgroundImage = "image");
