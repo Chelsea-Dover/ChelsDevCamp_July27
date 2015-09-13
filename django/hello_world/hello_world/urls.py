f8416fc5f47e899abaa9940cf6efe366a2f2969d
Chelsea <Chelseamegandover@gmail.com>
2015-09-12 20:59:07 -0700

    WHY WONT YOU COMMIT

--- django/hello_world/hello_world/urls.py
+++ django/hello_world/hello_world/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', views.index, name='index'),
-    url(r'^corgi.html', views.corgi, name='corgis'),
-    url(r'^forum.html', views.forum, name='forum')
+    url(r'^hello/', include('hello.urls')),
 ]
